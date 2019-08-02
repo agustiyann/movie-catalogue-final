6d15d709939cab88223701632a9c7e2a5723dd38
agustiyann <agustiyaninfo@gmail.com>
2019-08-02 14:35:25 +0700

    :zap: Migrate to AndroidX :rocket: :rocket:

--- app/src/main/java/com/atsdev/moviecataloguedb/MainActivity.java
+++ app/src/main/java/com/atsdev/moviecataloguedb/MainActivity.java
@@ -3,9 +3,9 @@ package com.atsdev.moviecataloguedb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
--- app/src/main/java/com/atsdev/moviecataloguedb/details/DetailTvShowActivity.java
+++ app/src/main/java/com/atsdev/moviecataloguedb/details/DetailTvShowActivity.java
@@ -4,7 +4,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
